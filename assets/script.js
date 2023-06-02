705638cd9107332f833decd3d67da8a6a0894d89
Laura <mobttcode@gmail.com>
2023-06-02 20:38:24 +0200

    clean

--- assets/script.js
+++ assets/script.js
@@ -20,65 +20,58 @@ const slides = [
 
 const leftArrow = document.querySelector(".arrow_left")
 const rightArrow = document.querySelector(".arrow_right")
-
-
+const dotsContainer = document.querySelector(".dots");
 let slideIndex = 0;
 let slideCount = slides.length;
 
 
-function slideInfo () {
-	const slideImg = document.querySelector(".banner-img");
-	const slideTxt = document.querySelector("#banner p");
-
-	slideImg.src = slides[slideIndex].image
- 	slideTxt.innerHTML = slides[slideIndex].tagLine
-
-	dotArray[activeDot].classList.remove("dot_selected")
-	activeDot = slideIndex
-	dotArray[activeDot].classList.add("dot_selected");
-}
-
-
+// CRÉATION DES DOTS EN FONCTION DU NOMBRE DE SLIDES
 for (let i = 0; i < slideCount; i++) {
-	const dotsContainer = document.querySelector(".dots");
 	const dot = document.createElement("span");
 	dot.classList.add("dot");
 	dotsContainer.appendChild(dot);
 }
 
+
+// DOT ACTIVE
+let dotArray = document.querySelectorAll(".dot");
 let activeDot = 0
-let dotArray = document.querySelectorAll(".dots .dot");
 dotArray[activeDot].classList.add("dot_selected");
 
+function activeDotChange() {
+	dotArray.forEach(dotArray => dotArray.classList.remove("dot_selected"));
+	dotArray[slideIndex].classList.add("dot_selected");
+}
 
 
+// IMAGES ET TEXTES
+function slideInfo () {
+	const slideImg = document.querySelector(".banner-img");
+	const slideTxt = document.querySelector("#banner p");
+	slideImg.src = slides[slideIndex].image
+ 	slideTxt.innerHTML = slides[slideIndex].tagLine
+}
 
 
+// SLIDE CLIC GAUCHE
 leftArrow.addEventListener("click", (event) => {
 	--slideIndex;
-
 	if (slideIndex < 0) {
 		slideIndex = slideCount -1;
 	}
 
 	slideInfo();
-
-	console.log('clic gauche test')
-	console.log("slideIndex " + slideIndex)
-	console.log("slideCount " + slideCount)
+	activeDotChange();
 })
 
 
+// SLIDE CLIC DROIT
 rightArrow.addEventListener("click", (event) => {
 	++slideIndex;
-
 	if (slideIndex > slideCount -1) {
 		slideIndex = 0;
 	}
 
 	slideInfo();
-
-	console.log('clic droit test')
-	console.log("slideIndex " + slideIndex)
-	console.log("slideCount " + slideCount)
+	activeDotChange();
 })
